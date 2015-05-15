3385b1f778f288682fe5642f21b76b00b233fbde
leroi <leroidejesa@gmail.com>
2015-05-15 10:15:22 -0700

    add initial index page code; add routes for adding new bands, including form; create various views files, empty

--- app.rb
+++ app.rb
@@ -2,3 +2,31 @@ require("bundler/setup")
 Bundler.require(:default, :production)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @bands = Band.all()
+  @venues = Venue.all()
+  erb(:index)
+end
+
+get('/bands/new') do
+  erb(:band_form)
+end
+
+post('/bands') do
+  name = params.fetch('name')
+  @new_band = Band.new({:description => name})
+  if @new_band.save()
+    redirect to("/bands/#{@new_band.id}")
+  else
+    @bands = Band.all()
+    erb(:index)
+  end
+end
+
+get('/bands/:id') do
+  @band = Band.find(params.fetch("id").to_i())
+  @venues = Venue.all()
+  @bands_venues = @band.venues()
+  erb(:band)
+end
